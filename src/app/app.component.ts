2834dc8e051ebf8e6764732cac7162b38a909ff1
greg-hammond <45513541+greg-hammond@users.noreply.github.com>
2019-10-31 18:51:20 -0500

    style property binding example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quiz-editor';
+
+  //propName = 'Purple';
+  propName = Math.random() > 0.5 ? 'blue' : 'pink' ;
+
+  borderRadius = '8px';
+
+
 }
