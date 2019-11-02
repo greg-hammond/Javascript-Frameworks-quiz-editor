08e8961e99eacc09928c2c9fea376130c8ff0be6
greg-hammond <45513541+greg-hammond@users.noreply.github.com>
2019-11-02 09:49:53 -0500

    clean up prior to slack-n-tell work
    get rid of unneeded stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,32 +7,23 @@ import { QuizService } from './quiz.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'quiz-editor';
 
-  //propName = 'Purple';
-  propName = Math.random() > 0.5 ? 'blue' : 'pink' ;
-
-  borderRadius = '8px';
-
-  toolTipText = `The color is ${this.propName}`;
-
-  someHtmlString = '<h1>attribute binding in da haus</h1>';
-
   quizzes = [];
+
+  selectedQuiz = undefined;
 
   // manually added constructor
   constructor(private qSvc: QuizService) {
 
     this.quizzes = qSvc.loadQuizzes();
-    console.log(this.quizzes);
 
   }
 
-  selectedQuiz = undefined;
 
   selectQuiz(q) {
     this.selectedQuiz = q;
-    console.log(this.selectedQuiz.name);
   }
 
 
