95934b359426c1fed8b3fe4ed0d9fd19733cbc15
greg-hammond <45513541+greg-hammond@users.noreply.github.com>
2019-11-15 19:25:27 -0600

    add ng-bootstrap
    npm install --save @ng-bootstrap/ng-bootstrap
    
    and then import the NgbModule....

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,6 +15,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule
     , FormsModule
     , HttpClientModule
+    , NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
